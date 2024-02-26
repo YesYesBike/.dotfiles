975d70a7a466e128b226617dd0aa1fbd2e4cf475
YesYesBike <156565534+YesYesBike@users.noreply.github.com>
2024-02-26 22:39:52 +0900

    changed colorscheme for st and nvim terminal

--- .config/nvim/lua/yesyesbike/set.lua
+++ .config/nvim/lua/yesyesbike/set.lua
@@ -14,22 +14,45 @@ vim.api.nvim_create_autocmd("TermOpen", {
         vim.opt.relativenumber = false
         vim.opt.signcolumn = "no"
         --keep terminal colorscheme
-        vim.g.terminal_color_0 = "#000000"
-        vim.g.terminal_color_1 = "#cd0000"
-        vim.g.terminal_color_2 = "#00cd00"
-        vim.g.terminal_color_3 = "#cdcd00"
-        vim.g.terminal_color_4 = "#0000ee"
-        vim.g.terminal_color_5 = "#cd00cd"
-        vim.g.terminal_color_6 = "#00cdcd"
-        vim.g.terminal_color_7 = "#e5e5e5"
-        vim.g.terminal_color_8 = "#7f7f7f"
-        vim.g.terminal_color_9 = "#ff0000"
-        vim.g.terminal_color_10 = "#00ff00"
-        vim.g.terminal_color_11 = "#ffff00"
-        vim.g.terminal_color_12 = "#5c5cff"
-        vim.g.terminal_color_13 = "#ff00ff"
-        vim.g.terminal_color_14 = "#00ffff"
-        vim.g.terminal_color_15 = "#ffffff"
+        --vim.g.terminal_color_0 = "#000000"
+        --vim.g.terminal_color_1 = "#cd0000"
+        --vim.g.terminal_color_2 = "#00cd00"
+        --vim.g.terminal_color_3 = "#cdcd00"
+        --vim.g.terminal_color_4 = "#0000ee"
+        --vim.g.terminal_color_5 = "#cd00cd"
+        --vim.g.terminal_color_6 = "#00cdcd"
+        --vim.g.terminal_color_7 = "#e5e5e5"
+        --vim.g.terminal_color_8 = "#7f7f7f"
+        --vim.g.terminal_color_9 = "#ff0000"
+        --vim.g.terminal_color_10 = "#00ff00"
+        --vim.g.terminal_color_11 = "#ffff00"
+        --vim.g.terminal_color_12 = "#5c5cff"
+        --vim.g.terminal_color_13 = "#ff00ff"
+        --vim.g.terminal_color_14 = "#00ffff"
+        --vim.g.terminal_color_15 = "#ffffff"
+        --vim.cmd{ cmd = 'highlight', args = { "Terminal guibg='#000000'" } }
+        --vim.cmd{ cmd = 'highlight', args = { "Terminal guifg='#e5e5e5'" } }
+
+        --Tango Dark
+        vim.g.terminal_color_0 = "#2E3436"
+        vim.g.terminal_color_1 = "#CC0000"
+        vim.g.terminal_color_2 = "#4E9A06"
+        vim.g.terminal_color_3 = "#C4A000"
+        vim.g.terminal_color_4 = "#3465A4"
+        vim.g.terminal_color_5 = "#75507B"
+        vim.g.terminal_color_6 = "#06989A"
+        vim.g.terminal_color_7 = "#D3D7CF"
+        vim.g.terminal_color_8 = "#555753"
+        vim.g.terminal_color_9 = "#EF2929"
+        vim.g.terminal_color_10 = "#8AE234"
+        vim.g.terminal_color_11 = "#FCE94F"
+        vim.g.terminal_color_12 = "#729FCF"
+        vim.g.terminal_color_13 = "#AD7FA8"
+        vim.g.terminal_color_14 = "#34E2E2"
+        vim.g.terminal_color_15 = "#EEEEEC"
+
+        vim.cmd{ cmd = 'highlight', args = { "Terminal guibg='#2E3436'" } }
+        vim.cmd{ cmd = 'highlight', args = { "Terminal guifg='#D3D7CF'" } }
     end,
 })
 
