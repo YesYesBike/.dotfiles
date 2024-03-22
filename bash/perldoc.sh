3ee1f90942bfa06f033524c2c68f526b76b6ec5c
YesYesBike <156565534+YesYesBike@users.noreply.github.com>
2024-03-23 02:21:39 +0900

    some little change

--- bash/perldoc.sh
+++ bash/perldoc.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#it needs to be fixed later...
 export PERLDOC_PAGER="less -+C "
-keyword=$(cat /tmp/perldoc)
-tmux renamew perldoc($keyword)
-perldoc -f $keyword
+title=$(cat /tmp/perldoc)
+tmux renamew "perldoc($title)"
+perldoc -f $title
