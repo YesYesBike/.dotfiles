61ed9178a1615bbd060ad10977aaddc8a2390249
YesYesBike <yesyesbike@gmail.com>
2026-02-15 13:53:15 +0900

    dwm: tmux init for 2nd session

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 firefox &
+tmux new -ds 2 -n 'SU' 'su; exec bash'
+tmux neww -t '2:' -n 'NET' 'nmtui; exec bash'
+tmux neww -t '2:' -n 'VOL' 'alsamixer; exec bash'
 st -e tmux new -A -s 1 &
 exit 0
